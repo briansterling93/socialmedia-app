0c857507cf70cf69665e5358caeff7151e698fd1
Brian <briansterling93@yahoo.com>
2020-12-30 02:42:13 -0700

    init configurement

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+
+// db.authenticate(async () => {
+//   try {
+//     console.log("db connected");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
+const app = express();
+
+app.use(express.json({ extended: false })); //body parser
+
+app.get("/", (req, res) => {
+  res.send("heyyyyy");
+});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`App running on Port ${PORT}`);
+});
